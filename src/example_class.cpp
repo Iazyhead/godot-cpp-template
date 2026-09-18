d51be49ee44b77309ff8485e7d48202a8651b2cf
Iazyhead <107071013+Iazyhead@users.noreply.github.com>
2026-09-18 13:59:13 +0900

    Update example_class.cpp

--- src/example_class.cpp
+++ src/example_class.cpp
@@ -1,43 +1,19 @@
-#include <godot_cpp/classes/ref_counted.hpp>
+#include "example_class.h"
+
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
 
 using namespace godot;
 
-class GDExtExample : public RefCounted {
-    GDCLASS(GDExtExample, RefCounted)
-
-protected:
-    static void _bind_methods() {
-        ClassDB::bind_method(D_METHOD("say_hello"), &GDExtExample::say_hello);
-    }
-
-public:
-    String say_hello() {
-        return "Hello from GDExtension";
-    }
-};
-
-void initialize_gdext_example(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) return;
-    ClassDB::register_class<GDExtExample>();
+void ExampleClass::_bind_methods() {
+    ClassDB::bind_method(D_METHOD("print_type", "variant"), &ExampleClass::print_type);
+    ClassDB::bind_method(D_METHOD("say_hello"), &ExampleClass::say_hello);
 }
 
-void uninitialize_gdext_example(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) return;
+void ExampleClass::print_type(const Variant &p_variant) const {
+    print_line(vformat("Type: %d", p_variant.get_type()));
 }
 
-extern "C" {
-    GDExtensionBool GDE_EXPORT example_library_init(
-        GDExtensionInterfaceGetProcAddress p_get_proc_address,
-        const GDExtensionClassLibraryPtr p_library,
-        GDExtensionInitialization *r_initialization
-    ) {
-        godot::GDExtensionBinding::InitObject init_obj(
-            p_get_proc_address, p_library, r_initialization);
-        init_obj.register_initializer(initialize_gdext_example);
-        init_obj.register_terminator(uninitialize_gdext_example);
-        init_obj.set_minimum_library_initialization_level(
-            MODULE_INITIALIZATION_LEVEL_SCENE);
-        return init_obj.init();
-    }
+String ExampleClass::say_hello() {
+    return "Hello from GDExtension";
 }
