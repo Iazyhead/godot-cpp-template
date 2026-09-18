d18bd26ff29235ea1fdf21ce69651bb88d21f5b8
Iazyhead <107071013+Iazyhead@users.noreply.github.com>
2026-09-18 13:28:32 +0900

    Update example_class.cpp

--- src/example_class.cpp
+++ src/example_class.cpp
@@ -1,9 +1,43 @@
-#include "example_class.h"
+#include <godot_cpp/classes/ref_counted.hpp>
+#include <godot_cpp/core/class_db.hpp>
 
-void ExampleClass::_bind_methods() {
-	godot::ClassDB::bind_method(D_METHOD("print_type", "variant"), &ExampleClass::print_type);
+using namespace godot;
+
+class GDExtExample : public RefCounted {
+    GDCLASS(GDExtExample, RefCounted)
+
+protected:
+    static void _bind_methods() {
+        ClassDB::bind_method(D_METHOD("say_hello"), &GDExtExample::say_hello);
+    }
+
+public:
+    String say_hello() {
+        return "Hello from GDExtension";
+    }
+};
+
+void initialize_gdext_example(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) return;
+    ClassDB::register_class<GDExtExample>();
 }
 
-void ExampleClass::print_type(const Variant &p_variant) const {
-	print_line(vformat("Type: %d", p_variant.get_type()));
+void uninitialize_gdext_example(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) return;
+}
+
+extern "C" {
+    GDExtensionBool GDE_EXPORT example_library_init(
+        GDExtensionInterfaceGetProcAddress p_get_proc_address,
+        const GDExtensionClassLibraryPtr p_library,
+        GDExtensionInitialization *r_initialization
+    ) {
+        godot::GDExtensionBinding::InitObject init_obj(
+            p_get_proc_address, p_library, r_initialization);
+        init_obj.register_initializer(initialize_gdext_example);
+        init_obj.register_terminator(uninitialize_gdext_example);
+        init_obj.set_minimum_library_initialization_level(
+            MODULE_INITIALIZATION_LEVEL_SCENE);
+        return init_obj.init();
+    }
 }
